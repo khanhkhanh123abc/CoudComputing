9316dd46d11e9708e5a369cd8886a5c2eda7c6dc
khanhkhanh123abc <101043113+khanhkhanh123abc@users.noreply.github.com>
2023-12-22 23:08:28 +0700

    update
    update

--- models/ManufacturerModel.js
+++ models/ManufacturerModel.js
@@ -1,31 +1,13 @@
-const mongoose = require('mongoose');
-
-const manufacturerSchema = new mongoose.Schema({
-    ManufacturerID: {
-        type: String,
-        required: [true, 'ManufacturerID is required'],
-        unique: true
-    },
-    Name: {
-        type: String,
-        required: [true, 'Name is required'],
-        trim: true,
-        maxlength: [100, 'Name should not be more than 100 characters']
-    },
-    Location: {
-        type: String,
-        required: [true, 'Location is required']
-    },
-    ContactInfo: {
-        type: String,
-        required: [true, 'Contact Info is required']
-        // You can add more specific validation for phone or email if needed
-    },
-    LogoURL: {
-        type: String,
-        required: [true, 'Logo URL is required'],
-        match: [/\.(jpeg|jpg|gif|png)$/, 'Logo URL must be a valid image file']
-    }
-});
-
-module.exports = mongoose.model('Manufacturer', manufacturerSchema);
+var mongoose = require('mongoose');
+var ManufacturerSchema = mongoose.Schema(
+   {
+      name: {
+         type: String,
+         required: true,
+         minlength: [3, 'brand name must be at least 3 characters'],
+         maxlength: 20
+      },
+      country: String
+   });
+ManufacturerModel = mongoose.model('manufacturers', ManufacturerSchema);
+module.exports = ManufacturerModel;
--- models/ToyModel.js
+++ models/ToyModel.js
@@ -1,37 +1,14 @@
-const mongoose = require('mongoose');
-
-const toySchema = new mongoose.Schema({
-    ToyID: {
-        type: String,
-        required: [true, 'ToyID is required'],
-        unique: true
-    },
-    Name: {
-        type: String,
-        required: [true, 'Name is required'],
-        trim: true,
-        maxlength: [100, 'Name should not be more than 100 characters']
-    },
-    Type: {
-        type: String,
-        required: [true, 'Type is required'],
-        enum: ['Action Figure', 'Puzzle', 'Doll', 'Educational', 'Other']
-    },
-    AgeGroup: {
-        type: String,
-        required: [true, 'Age Group is required'],
-        match: [/^\d+-\d+ years$/, 'Age Group should be in the format "x-y years"']
-    },
-    ImageURL: {
-        type: String,
-        required: [true, 'Image URL is required'],
-        match: [/\.(jpeg|jpg|gif|png)$/, 'Image URL must be a valid image file']
-    },
-    ManufacturerID: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Manufacturer',
-        required: [true, 'ManufacturerID is required']
-    }
+var mongoose = require('mongoose');
+var ToySchema = mongoose.Schema({
+   model:   String,
+   color: String,
+   image: String,
+   manufacturer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'manufacturers'  // 'manufacturers': collection
+   }
 });
+//Relationship : toys (many) - manufacturers (one)
 
-module.exports = mongoose.model('Toys', toySchema);
+var ToyModel = mongoose.model('toys', ToySchema); // 'toys' : collection
+module.exports = ToyModel;
